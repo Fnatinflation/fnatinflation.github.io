fa09876440c09cfb791dda2a097e15d2749b7ed5
Mathias Brændstrup Laursen <201708134@post.au.dk>
2020-09-02 14:27:57 +0200

    fixed compatibility

--- Components/Layout.js
+++ Components/Layout.js
@@ -1,11 +1,30 @@
 import Header from './Header';
+import React from 'react'
+import MediaQuery from 'react-responsive'
+
+const webStyle = {
+  margin: "0 auto", width: "65%", overflowY: "scroll", height: "100%", overflowX: "hidden", border: '1px solid #DDD', backgroundColor: "#F2F2F2"
+}
+const mobileStyle = {
+  margin: "0 auto", width: "100%", overflowY: "scroll", height: "100%", overflowX: "hidden", border: '1px solid #DDD', backgroundColor: "#F2F2F2"
+}
 const Layout = props => (
   <div>
     <Header />
-    <div style={{ backgroundColor: "#D9D9D9", paddingTop: "40px",height:"100vh" }}>
-      <div style={{ margin: "0 auto", width: "65%", overflowY: "scroll",height:"100%",overflowX:"hidden", border: '1px solid #DDD', backgroundColor: "#F2F2F2" }}>
-        {props.children}
-      </div>
+    <div style={{ backgroundColor: "#D9D9D9", paddingTop: "40px", height: "100vh" }}>
+
+      <MediaQuery maxDeviceWidth={1224}>
+        <div style={mobileStyle}>
+          {props.children}
+        </div>
+      </MediaQuery>
+      
+      <MediaQuery minDeviceWidth={1224}>
+        <div style={webStyle}>
+          {props.children}
+        </div>
+      </MediaQuery>
+
     </div>
     <link
       rel="stylesheet"
